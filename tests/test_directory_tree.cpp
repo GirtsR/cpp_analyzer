f1bcc375cf6187344dfa3d6d200d04c27eb735c1
Ģirts Rudzišs <girts.rudziss@testdevlab.com>
2018-05-08 16:43:54 +0300

    Added test cases with all testdir directories

--- tests/test_directory_tree.cpp
+++ tests/test_directory_tree.cpp
@@ -13,8 +13,42 @@ BOOST_AUTO_TEST_CASE(no_such_dir_exception)
     BOOST_CHECK_THROW(DirectoryTree tree(fs::path("")), boost::filesystem::filesystem_error);
 }
 
-BOOST_AUTO_TEST_CASE(testdir_size)
+BOOST_AUTO_TEST_CASE(ccc_dir)
+{
+    DirectoryTree tree(fs::path("../tests/testdir/aaa/ccc"));
+    BOOST_CHECK_EQUAL(tree.return_dirsize(), 18);
+    BOOST_CHECK_EQUAL(tree.return_total_sloc(), 0);
+    BOOST_CHECK_EQUAL(tree.return_total_cloc(), 6);
+}
+
+BOOST_AUTO_TEST_CASE(bbb_dir)
+{
+    DirectoryTree tree(fs::path("../tests/testdir/aaa/bbb"));
+    BOOST_CHECK_EQUAL(tree.return_dirsize(), 12);
+    BOOST_CHECK_EQUAL(tree.return_total_sloc(), 1);
+    BOOST_CHECK_EQUAL(tree.return_total_cloc(), 0);
+}
+
+BOOST_AUTO_TEST_CASE(aaa_dir)
+{
+    DirectoryTree tree(fs::path("../tests/testdir/aaa"));
+    BOOST_CHECK_EQUAL(tree.return_dirsize(), 6532);
+    BOOST_CHECK_EQUAL(tree.return_total_sloc(), 8);
+    BOOST_CHECK_EQUAL(tree.return_total_cloc(), 13);
+}
+
+BOOST_AUTO_TEST_CASE(empty_dir)
+{
+    DirectoryTree tree(fs::path("../tests/testdir/emptyfolder"));
+    BOOST_CHECK_EQUAL(tree.return_dirsize(), 0);
+    BOOST_CHECK_EQUAL(tree.return_total_sloc(), 0);
+    BOOST_CHECK_EQUAL(tree.return_total_cloc(), 0);
+}
+
+BOOST_AUTO_TEST_CASE(testdir)
 {
     DirectoryTree tree(fs::path("../tests/testdir"));
     BOOST_CHECK_EQUAL(tree.return_dirsize(), 48426);
+    BOOST_CHECK_EQUAL(tree.return_total_sloc(), 708);
+    BOOST_CHECK_EQUAL(tree.return_total_cloc(), 717);
 }
